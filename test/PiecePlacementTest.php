8b4c4c002439c5cc0ed6073b96def2311889224e
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2019-01-23 21:32:53 +0100

    Run php-cs-fixer

--- test/PiecePlacementTest.php
+++ test/PiecePlacementTest.php
@@ -1,32 +1,34 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+declare(strict_types=1);
+
+require __DIR__.'/../vendor/autoload.php';
 
 use \Ryanhs\Chess\Chess;
 
 class PiecePlacementTest extends \PHPUnit_Framework_TestCase
 {
-	public function testAll()
-	{
-		$chess = new Chess('8/8/8/8/8/8/8/8 w KQkq - 0 1');
-		
-		$this->assertSame($chess->get('a1'), null);
-		$this->assertSame($chess->get('a2'), null);
-		$this->assertSame($chess->get('e4'), null);
-		$this->assertSame($chess->get('g8'), null);
-		
-		$piece = ['type' => Chess::QUEEN, 'color' => Chess::WHITE];
-		$chess->put($piece, 'e4');
-		$this->assertSame($chess->get('e4'), $piece);
-		
-		$piece = ['type' => Chess::KING, 'color' => Chess::BLACK];
-		$chess->put($piece, 'd3');
-		$this->assertSame($chess->get('d3'), $piece);
-		
-		$chess->remove('d3');
-		$this->assertSame($chess->get('d3'), null);
-		
-		$chess->remove('d3');
-		$this->assertSame($chess->get('d3'), null);
-	}
+    public function testAll()
+    {
+        $chess = new Chess('8/8/8/8/8/8/8/8 w KQkq - 0 1');
+        
+        $this->assertSame($chess->get('a1'), null);
+        $this->assertSame($chess->get('a2'), null);
+        $this->assertSame($chess->get('e4'), null);
+        $this->assertSame($chess->get('g8'), null);
+        
+        $piece = ['type' => Chess::QUEEN, 'color' => Chess::WHITE];
+        $chess->put($piece, 'e4');
+        $this->assertSame($chess->get('e4'), $piece);
+        
+        $piece = ['type' => Chess::KING, 'color' => Chess::BLACK];
+        $chess->put($piece, 'd3');
+        $this->assertSame($chess->get('d3'), $piece);
+        
+        $chess->remove('d3');
+        $this->assertSame($chess->get('d3'), null);
+        
+        $chess->remove('d3');
+        $this->assertSame($chess->get('d3'), null);
+    }
 }
