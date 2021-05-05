8db4792a6282ef8ebe699e498b0e8e100947bfb2
lovephpforever <83395798+lovephpforever@users.noreply.github.com>
2021-05-05 05:25:01 -0400

    Use `DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts`.

--- test/PgnTest.php
+++ test/PgnTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require __DIR__.'/../vendor/autoload.php';
 
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
 use \Ryanhs\Chess\Chess;
 
 class PgnTest extends \PHPUnit\Framework\TestCase
