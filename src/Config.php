a6a9f3d208faa4da0c3db656b98b18e5dac2cd45
lovephpforever <83395798+lovephpforever@users.noreply.github.com>
2021-09-01 06:14:32 -0500

    Update and rename Bits.php to Config.php

---
+++ src/Config.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Chess.php
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 ryan hs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Ryanhs\Chess;
+
+class Config
+{
+    /** @const PIECE_BITS A list of piece bits to provide fast piece detection. */
+    public const PIECE_BITS = [
+        'pawn'   => 1,
+        'knight' => 2,
+        'bishop' => 4,
+        'queen'  => 8,
+        'king'   => 16,
+    ];
+
+    /** @const PIECE_BITS A list of move bits to provide fast move type detection. */
+    public const MOVE_BITS = [
+        'normal'     => 0,
+        'big_pawn'   => 1,
+        'capture'    => 2,
+        'ep_capture' => 4,
+        'qs_castle'  => 8,
+        'ks_castle'  => 16
+    ];
+
+    /** @const EMPTY_SQUARE Used to detect empty squares. */
+    public const EMPTY_SQUARE = 0;
+
+    /** @const PAWN_OFFSETS A list of piece movements for pawns. */
+    public const PAWN_OFFSETS = [
+        [ 16,  32,  17,  15],
+        [-16, -32, -17, -15],
+    ];
+
+    /** @const PAWN_OFFSETS A list of piece movements for other pieces. */
+    public const PIECE_OFFSETS = [
+        self::PIECE_BITS['knight'] => [-18, -33, -31, -14,  18,  33,  31,  14],
+        self::PIECE_BITS['bishop'] => [-17, -15,  17,  15],
+        self::PIECE_BITS['rook']   => [-16,   1,  16,  -1],
+        self::PIECE_BITS['queen']  => [-17, -16, -15,   1,  17,  16,  15,  -1],
+        self::PIECE_BITS['king']   => [-17, -16, -15,   1,  17,  16,  15,  -1],
+    ];
+
+    /** @const ATTACKS Used to detect piece attacks using bitwise operations. */
+    public const ATTACKS = [
+        20, 0, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0, 0,20, 0,
+         0,20, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0,20, 0, 0,
+         0, 0,20, 0, 0, 0, 0, 24,  0, 0, 0, 0,20, 0, 0, 0,
+         0, 0, 0,20, 0, 0, 0, 24,  0, 0, 0,20, 0, 0, 0, 0,
+         0, 0, 0, 0,20, 0, 0, 24,  0, 0,20, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0,20, 2, 24,  2,20, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 2,53, 56, 53, 2, 0, 0, 0, 0, 0, 0,
+        24,24,24,24,24,24,56,  0, 56,24,24,24,24,24,24, 0,
+         0, 0, 0, 0, 0, 2,53, 56, 53, 2, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0,20, 2, 24,  2,20, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0,20, 0, 0, 24,  0, 0,20, 0, 0, 0, 0, 0,
+         0, 0, 0,20, 0, 0, 0, 24,  0, 0, 0,20, 0, 0, 0, 0,
+         0, 0,20, 0, 0, 0, 0, 24,  0, 0, 0, 0,20, 0, 0, 0,
+         0,20, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0,20, 0, 0,
+        20, 0, 0, 0, 0, 0, 0, 24,  0, 0, 0, 0, 0, 0,20
+    ];
+
+    /** @const RAYS Used for fast check detection. */
+    public const RAYS = [
+         17,  0,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0,  0, 15, 0,
+          0, 17,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0, 15,  0, 0,
+          0,  0, 17,  0,  0,  0,  0, 16,  0,  0,  0,  0, 15,  0,  0, 0,
+          0,  0,  0, 17,  0,  0,  0, 16,  0,  0,  0, 15,  0,  0,  0, 0,
+          0,  0,  0,  0, 17,  0,  0, 16,  0,  0, 15,  0,  0,  0,  0, 0,
+          0,  0,  0,  0,  0, 17,  0, 16,  0, 15,  0,  0,  0,  0,  0, 0,
+          0,  0,  0,  0,  0,  0, 17, 16, 15,  0,  0,  0,  0,  0,  0, 0,
+          1,  1,  1,  1,  1,  1,  1,  0, -1, -1,  -1,-1, -1, -1, -1, 0,
+          0,  0,  0,  0,  0,  0,-15,-16,-17,  0,  0,  0,  0,  0,  0, 0,
+          0,  0,  0,  0,  0,-15,  0,-16,  0,-17,  0,  0,  0,  0,  0, 0,
+          0,  0,  0,  0,-15,  0,  0,-16,  0,  0,-17,  0,  0,  0,  0, 0,
+          0,  0,  0,-15,  0,  0,  0,-16,  0,  0,  0,-17,  0,  0,  0, 0,
+          0,  0,-15,  0,  0,  0,  0,-16,  0,  0,  0,  0,-17,  0,  0, 0,
+          0,-15,  0,  0,  0,  0,  0,-16,  0,  0,  0,  0,  0,-17,  0, 0,
+        -15,  0,  0,  0,  0,  0,  0,-16,  0,  0,  0,  0,  0,  0,-17
+    ];
+}
